1033236673e6531529db66eda01645deb80c2a2a
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2022-12-29 06:21:33 -0600

    Initial Commit

---
+++ NavigationExamples/NavigationExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationExamplesApp.swift
+//  NavigationExamples
+//
+//  Created by Justin Hold on 12/29/22.
+//
+
+import SwiftUI
+
+@main
+struct NavigationExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
